1939dc945e8592be5920e1e31e4f3e8ffcaf02be
Branden <brandenl8@gmail.com>
2021-06-14 23:54:52 +0900

    installed a lot of dependencies for the project. However running into some errors trying to install some of the special webpack plugins

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,6 @@ import {performAction} from './js/app';
 import {updateUI} from '.js/updateUI';
 
 import './styles/main.scss';
-
+import './img/SingaporeTrees.jpg';
 
 export {performAction, updateUI};
